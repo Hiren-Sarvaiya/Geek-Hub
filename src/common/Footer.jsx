0f740d426651699751b9b74ec6bb54036578a8b6
Hiren-Sarvaiya <hirensarvaiya1612@gmail.com>
2024-08-21 21:52:20 +0530

    Replaced Tailwind CSS with the normal CSS

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -1,16 +1,17 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
+import "../assets/css/Footer.css"
 
 const Footer = () => {
   return (
-    <footer className="px-4 py-2 bg-[#383FEF] flex justify-center items-center flex-col">
-      <div className="socialLinksContainer w-full flex justify-center gap-4 border-b-2 p-4">
-        <NavLink className="h-8 w-8 flex justify-center items-center bg-black p-5 rounded-full transition-all hover:text-[#1f7bf2] hover:bg-white" target="_blank" to="https://www.instagram.com/hiren_sarvaiya_16/"><i className="fab fa-facebook-f"></i></NavLink>
-        <NavLink className="h-8 w-8 flex justify-center items-center bg-black p-5 rounded-full transition-all hover:text-[#08a5f3] hover:bg-white" target="_blank" to="https://www.instagram.com/hiren_sarvaiya_16/"><i className="fab fa-twitter"></i></NavLink>
-        <NavLink className="h-8 w-8 flex justify-center items-center bg-black p-5 rounded-full transition-all hover:text-[#d73685] hover:bg-white" target="_blank" to="https://www.instagram.com/hiren_sarvaiya_16/"><i className="fab fa-instagram"></i></NavLink>
-        <NavLink className="h-8 w-8 flex justify-center items-center bg-black p-5 rounded-full transition-all hover:text-[#087bb9] hover:bg-white" target="_blank" to="https://www.instagram.com/hiren_sarvaiya_16/"><i className="fab fa-linkedin-in"></i></NavLink>
+    <footer className="flex justify-center items-center flex-col">
+      <div className="socialLinksContainer flex justify-center">
+        <NavLink className="footer-links cur-p flex justify-center items-center fb" target="_blank" to="https://www.instagram.com/hiren_sarvaiya_16/"><i className="fab fa-facebook-f"></i></NavLink>
+        <NavLink className="footer-links cur-p flex justify-center items-center twitter" target="_blank" to="https://www.instagram.com/hiren_sarvaiya_16/"><i className="fab fa-twitter"></i></NavLink>
+        <NavLink className="footer-links cur-p flex justify-center items-center ig" target="_blank" to="https://www.instagram.com/hiren_sarvaiya_16/"><i className="fab fa-instagram"></i></NavLink>
+        <NavLink className="footer-links cur-p flex justify-center items-center linkedin" target="_blank" to="https://www.instagram.com/hiren_sarvaiya_16/"><i className="fab fa-linkedin-in"></i></NavLink>
       </div>
-      <div className="copyrightSection w-full py-4 flex justify-between">
+      <div className="copyrightSection flex justify-between">
         <div>Hiren Sarvaiya - Creator</div>
         <div>2024 - All rights reserved</div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,48 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import "../assets/css/Home.css"
 
 const Home = () => {
+  const [blogsData, setBlogsData] = useState([])
+  const [gridRows, setGridRows] = useState(null)
+
+  useEffect(() => {
+    fetch("https://66c47ea4b026f3cc6cef9574.mockapi.io/api/v1/blogsData")
+      .then(res => { return res.json() })
+      .then(data => { setBlogsData(data) })
+  }, [])
+
+  useEffect(() => {
+    setGridRows(Math.ceil(blogsData.length / 3))
+  }, [blogsData])
+
+  const getFullDate = (dateString) => {
+    const dateObj = new Date(dateString)
+
+    const year = dateObj.getFullYear().toString()
+    const month = dateObj.toLocaleString("default", { month: "long" })
+    const date = dateObj.getDate().toString()
+
+    return `${date} ${month}, ${year}`
+  }
+
   return (
-    <main className="p-4">
-      Home
+    <main>
+      <h1>Blogs</h1>
+      <div style={{gridTemplateRows: `repeat(${gridRows}, 455px)`}} className="blogsContainer">
+        {blogsData.map(blog => {
+          return (
+            <div className="blogCard">
+              <img src={blog.image} />
+              <div className="title">{blog.title}</div>
+              <div className="desc">{blog.content}{blog.content}</div>
+              <div className="cardFooter flex justify-between">
+                <div className="date">{getFullDate(blog.creationDate)}</div>
+                <div className="author">{blog.author}</div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </main>
   )
 }
