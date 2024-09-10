51b44b12c7c02b14be642b0995e8d256088c6c53
Hiren-Sarvaiya <hirensarvaiya1612@gmail.com>
2024-09-10 21:46:41 +0530

    Backup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,31 @@
 import React from "react"
+import "../assets/css/Login.css"
+import { PiEye, PiEyeSlash } from "react-icons/pi"
+import { NavLink } from "react-router-dom"
 
 const Login = () => {
   return (
-    <div>
-      Log In
-    </div>
+    <section className="loginContainer flex justify-center">
+      <div className="formContainer">
+        <div className="bg"></div>
+        <form className="flex justify-center items-center flex-col">
+          <h1>Hello, Again... &#128075;</h1>
+          <div className="usernameContainer inputContainers">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" />
+          </div>
+          <div className="passwordContainer">
+            <div className="passwordContainer2 inputContainers">
+              <label htmlFor="password">Password</label>
+              <input type="text" id="password" />
+            </div>
+            <PiEyeSlash className="img cur-p" />
+          </div>
+          <input type="submit" value="Log In" />
+          <p>Don't have an account? <NavLink to="/signup">Sign Up</NavLink></p>
+        </form>
+      </div>
+    </section>
   )
 }
 
